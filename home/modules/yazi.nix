87de6de511e5c6145e6e74888ffbcd991bb60b07
skydive420dz <r0liveira@icloud.com>
2026-05-14 18:39:59 -0400

    Move Yazi config into home module

--- home/modules/yazi.nix
+++ home/modules/yazi.nix
@@ -8,10 +8,10 @@
   };
 
   xdg.configFile = {
-    "yazi/yazi.toml".source = ../../config/yazi/yazi.toml;
-    "yazi/keymap.toml".source = ../../config/yazi/keymap.toml;
-    "yazi/theme.toml".source = ../../config/yazi/theme.toml;
-    "yazi/package.toml".source = ../../config/yazi/package.toml;
-    "yazi/plugins".source = ../../config/yazi/plugins;
+    "yazi/yazi.toml".source = ./programs/yazi-config/yazi.toml;
+    "yazi/keymap.toml".source = ./programs/yazi-config/keymap.toml;
+    "yazi/theme.toml".source = ./programs/yazi-config/theme.toml;
+    "yazi/package.toml".source = ./programs/yazi-config/package.toml;
+    "yazi/plugins".source = ./programs/yazi-config/plugins;
   };
 }
