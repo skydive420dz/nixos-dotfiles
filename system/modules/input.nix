3ef0cb1461d60370d58192e7e4e058d9660d4692
skydive420dz <r0liveira@icloud.com>
2026-05-13 21:22:10 -0400

    nixos: add caps ctrl-alt layer

--- system/modules/input.nix
+++ system/modules/input.nix
@@ -24,4 +24,20 @@
   };
 
   services.libinput.enable = true;
+
+  services.keyd = {
+    enable = true;
+
+    keyboards.default = {
+      ids = [ "*" ];
+
+      settings = {
+        main = {
+          capslock = "overload(controlalt, esc)";
+        };
+
+        "controlalt:C-A" = { };
+      };
+    };
+  };
 }
