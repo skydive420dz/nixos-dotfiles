830125e2a2102364794b16c58ed7db0273fe5f4d
skydive420dz <r0liveira@icloud.com>
2026-05-14 18:31:30 -0400

    Use Yazi shell wrapper

--- home/modules/programs/zsh.nix
+++ home/modules/programs/zsh.nix
@@ -46,14 +46,6 @@
         tmux new -A -s main
       fi
 
-      function y() {
-        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-        yazi "$@" --cwd-file="$tmp"
-        if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-          builtin cd -- "$cwd"
-        fi
-        rm -f -- "$tmp"
-      }
     '';
   };
 }
--- home/modules/yazi.nix
+++ home/modules/yazi.nix
@@ -4,6 +4,7 @@
   programs.yazi = {
     enable = true;
     package = pkgs.yazi;
+    shellWrapperName = "y";
   };
 
   xdg.configFile = {
