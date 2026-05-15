e5b08ca458ecafbc55de03d2070d0cbf96acfef4
skydive420dz <r0liveira@icloud.com>
2026-05-14 21:45:02 -0400

    nixos: authorize local ssh key

--- system/modules/users.nix
+++ system/modules/users.nix
@@ -12,6 +12,7 @@
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIABClAXOeEh/2GHlwyKuAq2L3EY5sZYsw/I4HlLYKokm r0liveira@icloud.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFY+QfRli3gArIR6yXju7uMfgYfMlmVkTfsXbLw6zaVa skydive420dz@nixos"
     ];
     packages = with pkgs; [
       tree
