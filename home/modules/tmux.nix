60887a012e733fa55d77871396ab7024574b593c
Rafael Oliveira <r0liveira@icloud.com>
2026-05-18 19:20:41 -0400

    tmux: cleanup - keybinds

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -141,9 +141,9 @@ in
       # COPY MODE
       # =========================================
 
-      bind [ copy-mode
+      bind v copy-mode
 
-      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi Space send-keys -X begin-selection
       bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel 'wl-copy'
 
       # =========================================
