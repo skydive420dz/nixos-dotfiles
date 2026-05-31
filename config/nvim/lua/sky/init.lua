00d2e5813fa986cdf6102a5abcc45548a7b70760
skydive420dz <r0liveira@icloud.com>
2026-05-31 00:21:10 -0400

    Move nvim autocmds into live config

--- config/nvim/lua/sky/init.lua
+++ config/nvim/lua/sky/init.lua
@@ -2,6 +2,7 @@ local M = {}
 
 local modules = {
   "options",
+  "autocmds",
 }
 
 function M.setup()
--- system/modules/programs/nvim/lua.nix
+++ system/modules/programs/nvim/lua.nix
@@ -74,15 +74,6 @@
       vim.cmd("edit " .. vim.fn.fnameescape(vim.lsp.get_log_path()))
     end, { desc = "Open the Neovim LSP log" })
 
-    local yank_highlight_group = vim.api.nvim_create_augroup("UserYankHighlight", { clear = true })
-    vim.api.nvim_create_autocmd("TextYankPost", {
-      group = yank_highlight_group,
-      desc = "Highlight yanked text",
-      callback = function()
-        vim.hl.on_yank()
-      end,
-    })
-
     -- Neovim terminals inherit from the Neovim process, so recover the
     -- Hyprland instance when Neovim starts from a stale environment.
     if (vim.env.HYPRLAND_INSTANCE_SIGNATURE == nil or vim.env.HYPRLAND_INSTANCE_SIGNATURE == "") and vim.env.XDG_RUNTIME_DIR then
@@ -131,40 +122,6 @@
     vim.keymap.set("n", "<leader>zg", "zg", { desc = "Add word to dictionary" })
     vim.keymap.set("n", "<leader>zw", "zw", { desc = "Mark word wrong" })
     vim.keymap.set("n", "<leader>z=", "z=", { desc = "Spelling suggestions" })
-
-    -- Markdown link navigation
-    local markdown_navigation_group = vim.api.nvim_create_augroup("UserMarkdownNavigation", { clear = true })
-
-    vim.api.nvim_create_autocmd("FileType", {
-      group = markdown_navigation_group,
-      pattern = "markdown",
-      callback = function(event)
-        vim.opt_local.foldenable = true
-        vim.opt_local.foldlevel = 99
-
-        if vim.treesitter and vim.treesitter.foldexpr then
-          vim.opt_local.foldmethod = "expr"
-          vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-        else
-          vim.opt_local.foldmethod = "indent"
-        end
-
-        local link_target_pattern = [=[\[[^]]\+\](\zs[^)]\+)]=]
-
-        local function jump_link(flags)
-          return function()
-            local found = vim.fn.search(link_target_pattern, flags)
-            if found == 0 then
-              vim.notify("No markdown link found", vim.log.levels.INFO, { title = "Markdown" })
-            end
-          end
-        end
-
-        local opts = { buffer = event.buf, silent = true }
-        vim.keymap.set("n", "]u", jump_link("W"), vim.tbl_extend("force", opts, { desc = "Next markdown link" }))
-        vim.keymap.set("n", "[u", jump_link("bW"), vim.tbl_extend("force", opts, { desc = "Previous markdown link" }))
-      end,
-    })
 
     local function mini_files_open(path)
       return function()
