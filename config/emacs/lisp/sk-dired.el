b513408f725cc1aa9a562d7e3c119f9cb6219fce
Rafael Oliveira <r0liveira@icloud.com>
2026-06-06 11:17:24 -0400

    Tune Dired preview behavior

--- config/emacs/lisp/sk-dired.el
+++ config/emacs/lisp/sk-dired.el
@@ -21,14 +21,37 @@
   :after dired
   :commands (dired-preview-mode dired-preview-global-mode)
   :config
-  (setq dired-preview-delay 0.35
+  (setq dired-preview-delay 0.15
+        dired-preview-display-action-alist
+        '((display-buffer-in-side-window)
+          (side . bottom)
+          (window-height . 0.18)
+          (preserve-size . (nil . t)))
         dired-preview-max-size (expt 2 25)
         dired-preview-image-extensions-regexp
         "\\.\\(png\\|jpe?g\\|webp\\|gif\\|tiff?\\|svg\\|xpm\\|xbm\\|pbm\\)\\'"
         dired-preview-ignored-extensions-regexp
         (concat "\\."
                 "\\(gz\\|zst\\|tar\\|xz\\|rar\\|zip\\|iso\\|epub\\)"
-                "\\'")))
+                "\\'")
+        dired-preview-trigger-commands
+        '(dired-next-line
+          dired-previous-line
+          dired-flag-file-deletion
+          dired-mark
+          dired-unmark
+          dired-unmark-backward
+          dired-del-marker
+          dired-goto-file
+          dired-find-file
+          evil-next-line
+          evil-previous-line
+          evil-next-visual-line
+          evil-previous-visual-line
+          next-line
+          previous-line
+          scroll-up-command
+          scroll-down-command)))
 
 (provide 'sk-dired)
 
