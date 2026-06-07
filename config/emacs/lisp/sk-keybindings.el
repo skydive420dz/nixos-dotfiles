e78b35d3f95d32e4af52b4e4fea547039ee60c00
Rafael Oliveira <r0liveira@icloud.com>
2026-06-07 05:49:18 -0400

    Fix Emacs daemon restart race

--- config/emacs/lisp/sk-keybindings.el
+++ config/emacs/lisp/sk-keybindings.el
@@ -184,9 +184,9 @@
     (save-some-buffers)
     (let* ((unit (format "sk-emacs-restart-%s" (emacs-pid)))
            (client-command (if (executable-find "uwsm")
-                               "uwsm app -- emacsclient --create-frame --alternate-editor=emacs"
-                             "emacsclient --create-frame --alternate-editor=emacs"))
-           (restart-command (format "sleep 0.2; systemctl --user restart emacs.service; sleep 0.8; %s"
+                               "uwsm app -- emacsclient --create-frame --alternate-editor=false"
+                             "emacsclient --create-frame --alternate-editor=false"))
+           (restart-command (format "sleep 0.2; systemctl --user restart emacs.service; i=0; while [ \"$i\" -lt 80 ]; do emacsclient --alternate-editor=false --eval '(emacs-pid)' >/dev/null 2>&1 && exec %s; i=$((i + 1)); sleep 0.1; done; echo 'emacs daemon did not become ready' >&2; exit 1"
                                     client-command)))
       (unless (executable-find "systemd-run")
         (user-error "systemd-run is not available"))
