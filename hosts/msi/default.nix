61da209c4701b5124c47ad3cbdd0c4cebef095f3
Rafael Oliveira <r0liveira@icloud.com>
2026-08-28 05:55:42 -0400

    msi: add agent runtime tools

--- hosts/msi/default.nix
+++ hosts/msi/default.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ./hardware-configuration.nix
@@ -12,4 +14,9 @@
   hardware.keyboard.qmk.enable = true;
 
   zramSwap.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    bubblewrap
+    socat
+  ];
 }
